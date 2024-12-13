c60596b46102366efb5d2cd79a3ba0e67c44af55
myunghoon146 <myunghoon146@gmail.com>
2024-12-13 16:40:09 +0900

    두번째 파일입니다.

--- Program Set 1/src/Application1/Registry.java
+++ Program Set 1/src/Application1/Registry.java
@@ -1,7 +1,10 @@
 package Application1;
 
+import java.security.SecurityPermission;
+
 public class Registry {
     public static void main(String[] args) {
         System.out.println("Population Increase");
+        SecurityPermission perm ("Population Increase");
     }
 }
